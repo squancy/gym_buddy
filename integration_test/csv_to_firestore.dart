86bac294687c44d440a440deb7eddd0e3a2bb8fa
Mark Frankli <mark@pearscom.com>
2025-01-30 22:17:01 +0100

    added web scraped csv file and integration test to add the contents to firestore

---
+++ integration_test/csv_to_firestore.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gym_buddy/utils/helpers.dart' as helpers;
+import 'package:integration_test/integration_test.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:uuid/uuid.dart';
+
+// Import the web scraped csv files containing gyms in Hungary to the database
+
+void main() async {
+  test('Push scraped gyms to database', () async {
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    await helpers.firebaseInit(test: false);
+    final FirebaseFirestore db = FirebaseFirestore.instance;
+    final gymDocRef = db.collection('gyms').doc('budapest').collection('gyms');
+    
+    const int batchSize = 200; 
+    WriteBatch batch = db.batch();
+    int count = 0;
+
+    // First delete all documents in gyms/budapest/gyms
+    var snapshots = await gymDocRef.get();
+    for (var doc in snapshots.docs) {
+      count++;
+      batch.delete(doc.reference);
+      if (count % batchSize == 0) {
+        await batch.commit();
+        batch = db.batch();
+      }
+    }
+    await batch.commit();
+
+    final csvString = await rootBundle.loadString('assets/budapest_gyms.csv');
+    final fields = CsvToListConverter().convert(csvString);
+
+    // Add all gyms in the csv file
+    final uuid = Uuid();
+    count = 0;
+    batch = db.batch();
+
+    for (final (i, row) in fields.indexed) {
+      if (i == 0) {
+        continue; // first row in csv is the description: do not add it
+      }
+
+      count++;
+      String gymID = uuid.v4();
+      final gyms = gymDocRef.doc(gymID);
+      final data = {
+        'name': row[0],
+        'address': row[1],
+        'lat': row[3],
+        'lon': row[4]
+      };
+      batch.set(gyms, data);
+
+      if (count % batchSize == 0) {
+        await batch.commit();
+        batch = db.batch();
+      }
+    }
+
+    if (count % batchSize != 0) {
+      await batch.commit();
+    }
+  });  
+}
